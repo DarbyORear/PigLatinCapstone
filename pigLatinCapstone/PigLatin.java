fb40588216ac84ddfbacd58be8000cb58dcfa4a0
DarbyORear <darbyorear@gmail.com>
2018-07-22 23:18:46 -0400

    Made changes. Added a for loop and a do-while loop, etc.

--- pigLatinCapstone/PigLatin.java
+++ pigLatinCapstone/PigLatin.java
@@ -2,68 +2,61 @@ package pigLatinCapstone;
 import java.util.Scanner;
 
 public class PigLatin {
-
-
-	//main method
-		public static void main(String[] args) {
-			Scanner scnr = new Scanner(System.in);
-
-			char userWordFirstVowel='a';
-			
-			switch(userWordFirstVowel) {
-				case('a'):
-					translate('a');
-					break;
-				case('e'):
-					translate('e');
-					break;
-				case('i'):
-					translate('i');
-					break;
-				case('o'):
-					translate('o');
-					break;
-				case('u'):
-					translate('u');
-					break;
-				default:
-					break;
-					}
-//			System.out.println("Would you like to enter another word?");
-//			String userContinue = scnr.nextLine();
-//					
-//			if(userContinue.equals("yes") || userContinue.equals("y")) {
-//					System.out.println("Great! Let's play again.");
-//					} else {
-						System.out.println("Thanks for playing!");	
-					}
+	
+	//Main method:
+	public static void main(String[] args) {
 		
-					
-		public static void translate(char vowel){	
-				
-				String userWord="";
-				String newWord2;
-				String finalWord;
-				
-				Scanner scnr = new Scanner(System.in);
-				System.out.println("Welcome to the Pig Latin translator!" + "\n" + "\n"+ "Please enter a word that you would like to translate: ");
-				userWord = scnr.nextLine();	
+		Scanner scnr = new Scanner(System.in);
+		String userContinue;
+		
+		//Use a do-while loop to ask the user for input, translate the word into Pig Latin, and ask if they want to enter another word.
+		do {
+			translate();
 
-				if(userWord.contains("a") || userWord.contains("e") || userWord.contains("i")) {
-						userWord.indexOf(vowel);
-						String newWord = userWord.substring(userWord.indexOf(vowel), userWord.length());
-//						System.out.println(newWord);
-						newWord2 = userWord.substring(0, userWord.indexOf(vowel));
-//						System.out.println(newWord + newWord2);
-						finalWord = newWord + newWord2 + "ay";
-						System.out.println(finalWord);
-				}
-				else {
-					System.out.println("I don't think this is a word in English.");
-				}
-			scnr.close();
-			}
+			System.out.println("Would you like to enter another word? (yes/no): ");
+	
+			userContinue = scnr.next();
+			System.out.println();
 		}
+		
+		//set the "while" condition to when the user enters yes.
+			while(userContinue.equals("yes") || userContinue.equals("y"));
+		
+		//If the user doesn't want to continue, thank them for playing.
+		System.out.println("Thanks for playing!");
+	}
+
+	//Create a method called "translate()" to translate the user's word into Pig Latin:
+	public static void translate() {
+		
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Please enter a word: ");
+			
+		String userWord = scnr.next();
+		char firstVowel=0;
+		String newWord;
+		String newWord2;
+		String finalWord;
+		
+		//Create a for loop to scan the user's word for a vowel. Break after finding the first vowel in the word.
+		for(int i=0; i <= userWord.length()-1; i++){
+			if (userWord.charAt(i) == 'a' || userWord.charAt(i) == 'e' || userWord.charAt(i) == 'i' || userWord.charAt(i) == 'o' || userWord.charAt(i) == 'u') {
+				firstVowel = userWord.charAt(i);
+				break;
+			} 
+		}
+		
+		//Translate word to Pig Latin.
+		userWord.indexOf(firstVowel);
+		newWord = userWord.substring(userWord.indexOf(firstVowel), userWord.length());
+		newWord2 = userWord.substring(0, userWord.indexOf(firstVowel));
+		finalWord = newWord + newWord2 + "ay";
+		System.out.println(finalWord);
+		System.out.println();
+			
+	}
+
+}
 
 				
 				
